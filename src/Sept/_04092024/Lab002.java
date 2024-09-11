918219856d98d0ccfed0ae16675a4136ae21f98a
sam1-art <sambagade96@gmail.com>
2024-09-12 00:15:42 +0530

    hi

--- src/Sept/_04092024/Lab002.java
+++ src/Sept/_04092024/Lab002.java
@@ -1,4 +1,13 @@
 package Sept._04092024;
 
 public class Lab002 {
+
+    public static void main(String[] args) {
+        System.out.println(2+2);
+        System.out.println(2-2);
+        System.out.println(2*2);
+        System.out.println(2/2);
+
+        //lab 2
+    }
 }
