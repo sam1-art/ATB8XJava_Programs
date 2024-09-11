ba5c43fd22f31edf50bb19ac06785c190f158627
sam1-art <sambagade96@gmail.com>
2024-09-12 00:06:23 +0530

    hi

--- src/Sept/_04092024/Lab001.java
+++ src/Sept/_04092024/Lab001.java
@@ -1,4 +1,11 @@
 package Sept._04092024;
 
+import java.sql.SQLOutput;
+
 public class Lab001 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Samiksha!");
+        //first program
+    }
 }
