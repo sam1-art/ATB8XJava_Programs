2ac25534722510f1c93e0bfb47e0db6fd76e19d4
sam1-art <sambagade96@gmail.com>
2024-10-09 00:08:39 +0530

    task done

---
+++ src/Sept/_25092024/Task1.java
@@ -0,0 +1,12 @@
+package Sept._25092024;
+
+//Table of number n = 10, print table with For or while.
+public class Task1 {
+    public static void main(String[] args) {
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println(10+ "*" + i + "=" + 10*i);
+        }
+
+    }
+}
---
+++ src/Sept/_25092024/Task2.java
@@ -0,0 +1,16 @@
+package Sept._25092024;
+//Factorial Program - n = 5 , Output = 120 //  n = 5 ->  Fact = 5*4*3*2*1 = 120
+// n = 1 ->  Fact = 1*1 -> 1
+// n = 3 -> Fact = 3*2*1 -> 6
+public class Task2 {
+    public static void main(String[] args) {
+        int fact= 1;
+        int num=5;
+        for(int i=1; i<=num;i++)
+        {
+            fact=fact*i;
+        }
+        System.out.println(fact);
+    }
+
+}
