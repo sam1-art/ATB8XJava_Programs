cd4cc65835cb1efc44184cd9fd5d7f322744d415
sam1-art <sambagade96@gmail.com>
2024-10-09 16:24:09 +0530

    task done

---
+++ src/Sept/Sept_27092024/Lab01.java
@@ -0,0 +1,13 @@
+package Sept.Sept_27092024;
+import java.util.Scanner;
+
+//User Input by using scanner
+
+public class Lab01 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the number");
+        int num=sc.nextInt();
+        System.out.println(num);
+    }
+}
